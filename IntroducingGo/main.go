f418473f8135876bcfdd2846a614c1f94cf5c59d
Alex Petrushchak <petrushchako@gmail.com>
2025-04-17 23:07:17 +0100

    Add strings manipulation examples

--- IntroducingGo/main.go
+++ IntroducingGo/main.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main(){
 		fmt.Println("1 + 1 =", 1 + 1) // 2
 		fmt.Println("1.0 + 1.0 =", 1.0 + 1.0) // 2
 		stringOperations("Hello World")
+		nextLetter("Hello World")
+		spliter("Republic of Ireland")
 }
 
 
@@ -14,4 +19,14 @@ func stringOperations(x string){
 	fmt.Println("len(x):\t\t", len(x))
 	fmt.Println("x[0]:\t\t",x[0])
 	fmt.Println("string(x[0]):\t", string(x[0]))
+}
+
+func nextLetter(x string){
+	y := x[0]
+	fmt.Println(string(y+1))
+}
+
+func spliter(x string){
+	splited := strings.Split(x, "")
+	fmt.Println(splited)
 }
