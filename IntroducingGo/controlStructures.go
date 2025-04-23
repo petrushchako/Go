f34312854103262b57d8276d9511ac1481c37434
Alex Petrushchak <petrushchako@gmail.com>
2025-04-23 23:25:54 +0100

    Add if switch example

--- IntroducingGo/controlStructures.go
+++ IntroducingGo/controlStructures.go
@@ -2,15 +2,34 @@ package main
 import "fmt"
 
 func main(){
+    forLoop1()
+    forLoop2()
+
+    ifSwitch()
+}
+
+func forLoop1 (){
     i := 1
     for i <=10 {
         fmt.Print(i, " ")
         i++
     }
     fmt.Println()
+}
 
+func forLoop2(){
     for i:=10; i >0; i-- {
         fmt.Print(i, " ")
     }
     fmt.Println()
+}
+
+func ifSwitch(){
+    for i:=1; i <= 10; i++  {
+        if i % 2 ==0{
+            fmt.Println(i, " : even ")
+        }else{
+            fmt.Println(i, " : odd")
+        }
+    }
 }
