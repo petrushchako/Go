4154c233ece3362aae7d9b5e40656c0b7fef801f
Alex Petrushchak <petrushchako@gmail.com>
2025-04-23 23:15:51 +0100

    Add alternative for loop example

--- IntroducingGo/controlStructures.go
+++ IntroducingGo/controlStructures.go
@@ -4,7 +4,13 @@ import "fmt"
 func main(){
     i := 1
     for i <=10 {
-        fmt.Println(i)
+        fmt.Print(i, " ")
         i++
     }
+    fmt.Println()
+
+    for i:=10; i >0; i-- {
+        fmt.Print(i, " ")
+    }
+    fmt.Println()
 }
