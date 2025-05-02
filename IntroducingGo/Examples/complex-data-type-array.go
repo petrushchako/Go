08ce5f60cbf83cf6e36a355d142ba196a97839be
Alex Petrushchak <petrushchako@gmail.com>
2025-05-02 18:00:43 +0100

    Organize complex data type working files

--- IntroducingGo/Examples/complex-data-type-array.go
+++ IntroducingGo/Examples/complex-data-type-array.go
@@ -1,22 +1,22 @@
 package main
+
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("Arrays Examples:")
 	arraysExample1()
 	arraysExample2()
 	calculateSum([5]float64{12.23, 23.34, 34.45, 45.56, 56.67})
 }
 
-
-func arraysExample1(){
+func arraysExample1() {
 	var arr [2]string
 	arr[0] = "Oleksandr"
 	arr[1] = "Petrushchak"
-	fmt.Println(arr[0]+ " " + arr[1])
+	fmt.Println(arr[0] + " " + arr[1])
 }
 
-func arraysExample2(){
+func arraysExample2() {
 	var arr [8]int
 	arr[0] = 1
 	arr[2] = 1
@@ -25,20 +25,20 @@ func arraysExample2(){
 	fmt.Println(arr, "8-bit binary number is", calculate10Base(arr), "in 10 base")
 }
 
-func calculate10Base( bits [8]int) int{
+func calculate10Base(bits [8]int) int {
 	// (1 << (7 - i)) in Go << is the bitwise left shift operator.
 	// 1 << n means: take 1 and shift it left by n positions.
 	// Shifting left by n bits is the same as multiplying by 2ⁿ.
 	var result int
-	for i:=0; i < 8; i++ {
+	for i := 0; i < 8; i++ {
 		result += bits[i] * (1 << (7 - i))
 	}
 	return result
 }
 
-func calculateSum(arr [5]float64){
+func calculateSum(arr [5]float64) {
 	var total float64
-	for i:=0; i<5; i++{
+	for i := 0; i < 5; i++ {
 		total += arr[i]
 	}
 	fmt.Println("Total sum of", arr, "is", total)
